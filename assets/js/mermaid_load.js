0750349deea0a6268fb44de6627f48a9a301cf93
enidev911 <enidev911@gmail.com>
2024-05-12 00:47:49 -0400

    deleted

--- assets/js/mermaid_load.js
+++
@@ -1,9 +0,0 @@
-
-
-// mermaid.initialize({startOnLoad: false})
-await mermaid.run({
-  querySelector: ".mermaid",
-  postRenderCallback: (id) => {
-    document.querySelector(".mermaid").style.visibility = 'visible';
-  }
-})
